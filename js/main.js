14c77912125a0de01c57813bbe150091b77573fb
jpcbdev <jpcbdev@gmail.com>
2018-11-13 18:47:41 -0600

    formulario de contacto agregado

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-function showHome(){
+function showHome() {
     document.getElementById("home").hidden = false;
     document.getElementById("blog").hidden = true;
     document.getElementById("contact").hidden = true;
 
 }
 
-function showBlog(){
+function showBlog() {
     document.getElementById("home").hidden = true;
     document.getElementById("blog").hidden = false;
     document.getElementById("contact").hidden = true;
 }
-function showContact(){
+function showContact() {
     document.getElementById("home").hidden = true;
     document.getElementById("blog").hidden = true;
     document.getElementById("contact").hidden = false;
+}
+function sendData() {
+    if (document.getElementById("acepto").checked === false) {
+        throw alert("Por favor acepte los terminos");
+    }
+    if (document.getElementById("email").value.trim() === ""){
+        throw alert("Por Ingrese su correo");
+    }
+    alert("Datos enviados de forma exitosa");
+
+
 }
